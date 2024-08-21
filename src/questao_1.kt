063c1954394c02d0a374e9cb4a41f9070f032916
Maykon Silva <140466040+maykon3@users.noreply.github.com>
2024-08-20 23:48:37 -0300

    first commit

---
+++ src/questao_1.kt
@@ -0,0 +1,6 @@
+//Escreva um programa que crie uma variável chamada "nome_do_carro" e atribua-a um valor "Fusca". Exiba-a ao usuário.
+
+fun main() {
+    var nome_do_carro = "Fusca"
+    println(nome_do_carro)
+}
---
+++ src/questao_4_paralelogramo.kt
@@ -0,0 +1,11 @@
+fun main() {
+
+    print("Digite o valor da sua base: ")
+        var base: Double = readln().toDouble()
+
+    print("Digite o valor da altura:")
+        var altura: Double = readln().toDouble()
+
+    println("resultado do paralelogramo e: " + (base * altura))
+
+}

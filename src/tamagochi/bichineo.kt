3455085978c901003aa08dd4117d3008db3676f1
Maykon Silva <140466040+maykon3@users.noreply.github.com>
2024-11-01 19:53:16 -0300

    commit

--- src/tamagochi/bichineo.kt
+++ src/tamagochi/bichineo.kt
@@ -1,15 +1,15 @@
 package tamagochi
 
-class Tamagochi( var nome: String, var fome: Int = 10, var feliz: Int = 20, var cansaco: Int = 0, var idade: Int = 2, var vida: Boolean = true) {
+class Tamagochi( var nome: String, var fome: Int = 0, var feliz: Int = 100, var cansaco: Int = 0, var idade: Int = 2, var vida: Boolean = true) {
 
     fun comer() {
-        fome = fome - 4
+        fome = fome - 50
         println("Minha fome diminuiu um pouco!!!")
     }
 
     fun brinks(){
-        feliz = feliz + 3
-        cansaco = cansaco + 5
+        feliz = feliz + 10
+        cansaco = cansaco + 10
         fome = fome + 5
         println("Me sinto mais feliz.")
     }
@@ -17,11 +17,23 @@ class Tamagochi( var nome: String, var fome: Int = 10, var feliz: Int = 20, var 
     fun status (){
         println("$nome \n Fome: $fome\n Felicidade: $feliz\n Cansaço: $cansaco\n Idade: $idade")
     }
+    fun descanso (){
+        print("Quanto tempo $nome descansou ? ")
+        var dormes = readln().toInt()
+        if (dormes == 8){
+        cansaco = 0
+        }else if (dormes < 8){
+            cansaco = (dormes * 5)- cansaco
+        }else if ( dormes > 8 || dormes < 0 ){
+            println("Valor invalido")
+            descanso()
+        }
+    }
 
     fun futuro() {
-        fome += 2
-        cansaco += 2
-        feliz -= 3
+        fome += 30
+        cansaco += 10
+        feliz -= 30
         idade += 1
     }
 
--- src/tamagochi/tamagochi.kt
+++ src/tamagochi/tamagochi.kt
@@ -5,15 +5,16 @@ import tamagochi.Tamagochi
 fun main() {
     println("Bem vindo ao Tamagochi!")
     print(" Insira o nome de seu pet: ")
-    val nome = readln()
-    var vulgo = Tamagochi(nome)
+    val nome = readln() // atribui um nome para o tamagochi que estava sem
+    var vulgo = Tamagochi(nome) //vulgo faz a conecxao com a class e o main
 
     while (true) {
         println("\nEscolha uma ação:")
-        println("1. Alimentar $nome")
+        println("1. Alimentar o $nome")
         println("2. Brincar com $nome")
-        println("3. Status do $nome")
-        println("4. Sair")
+        println("3. Verificar o Status do $nome")
+        println("4. Descansar o $nome")
+        println("5. Sair")
 
         var escolha = readln().toIntOrNull()
 
@@ -21,20 +22,25 @@ fun main() {
             1 -> vulgo.comer()
             2 -> vulgo.brinks()
             3 -> vulgo.status()
-            4 -> {
+            4 -> vulgo.descanso()
+            5 -> {
                 println("Encerrando tamagochi...")
 
                 return
             }
 
         }
+        if (escolha != 3 ) {
+            vulgo.futuro()
+        }
 
-        vulgo.futuro()
-
-        if(vulgo.fome > 20 || vulgo.cansaco > 25){
+        if(vulgo.fome > 99 || vulgo.cansaco > 99 || vulgo.feliz <= 0){
             vulgo.vida = false
-            println("Infelizmente $nome foi de vasco...")
+            println("Infelizmente $nome foi de Comes e Bebes...")
             return
+        }else if ( vulgo.idade > 49){
+            println("Você alcançou o objetivo do Tamagochi")
+            vulgo.vida = false
         }
     }
 }
